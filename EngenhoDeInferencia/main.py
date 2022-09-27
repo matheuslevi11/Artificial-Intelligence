ec5654c440e13c22ffc91cd5dbc09c61ed8dae65
Matheus Levi <matheuslevi.aidano@gmail.com>
2022-09-27 00:42:08 -0300

    Adiciona novos tipos de encadeamento

--- EngenhoDeInferencia/main.py
+++ EngenhoDeInferencia/main.py
@@ -2,12 +2,24 @@ import pandas as pd
 import re
 from utils import importKnowledgeBase, read_question, scan
 
+def parse_rule_forward(rule):
+    rule = rule.split(' ')
+    for i in range(len(rule)):
+        e = rule[i]
+        if e != 'and' and e != 'or':
+            if e in base_fatos.FATO.to_list():
+                value = base_fatos[base_fatos.FATO == e].values[0][1]
+                rule[i] = value
+            else:
+                rule[i] = False
+    return rule
+
 def parse_rule(rule):
     rule = rule.split(' ')
     for i in range(len(rule)):
         e = rule[i]
         if e != 'and' and e != 'or':
-            rule[i] = infer(e)
+            rule[i] = infer_backwards(e)
     return rule
 
 def evaluate_expressions(exps):
@@ -34,7 +46,7 @@ def evaluate_expressions(exps):
     
     return exps[0]
             
-def infer(goal):
+def infer_backwards(goal):
     global base_fatos, base_regras
 
     if goal in base_fatos.FATO.to_list():
@@ -55,6 +67,40 @@ def infer(goal):
     
     return True if value == 'True' else False
 
+def infer_forwards():
+    global base_fatos, base_regras
+    rules_to_apply = [-1]
+    
+    while len(rules_to_apply) > 0:
+        rules_to_apply = []
+        for i, r in base_regras.iterrows():
+            exp = parse_rule_forward(r['ANTECEDENTE'])
+            antecedent = evaluate_expressions(exp)
+            consequent = r['CONSEQUENTE']
+            if antecedent and consequent not in base_fatos.FATO.to_list():
+                rules_to_apply.append(consequent)
+        
+        for rule in rules_to_apply:
+            print(f'Foi descoberto que {rule} é verdade.')
+            base_fatos.loc[len(base_fatos.index)] = [rule, 'True']
+
+def infer_mixed():
+    global base_fatos, base_regras
+    rules_to_apply = [-1]
+    
+    while len(rules_to_apply) > 0:
+        rules_to_apply = []
+        for i, r in base_regras.iterrows():
+            exp = parse_rule(r['ANTECEDENTE'])
+            antecedent = evaluate_expressions(exp)
+            consequent = r['CONSEQUENTE']
+            if antecedent and consequent not in base_fatos.FATO.to_list():
+                rules_to_apply.append(consequent)
+        
+        for rule in rules_to_apply:
+            print(f'Foi descoberto que {rule} é verdade.')
+            base_fatos.loc[len(base_fatos.index)] = [rule, 'True']
+
 if __name__ == '__main__':
     print('Bem vindo a Máquina de Inferência')
     print('Para alimentar a base de conhecimento, adicione os dados nos arquivos da pasta BaseDeConhecimento')
@@ -67,5 +113,12 @@ if __name__ == '__main__':
     q = read_question(fatos)
     global base_fatos, base_regras
     base_fatos, base_regras = importKnowledgeBase()
-    answer = infer(q)
-    print(f'A resposta é: {answer}')
+    print('Gostaria de usar encadeamento para frente ou para trás?')
+    choice = input('[1] Para Frente\n[2] Para trás\n[3] Misto')
+    if choice == '1':
+        infer_forwards()
+    elif choice == '2':
+        answer = infer_backwards(q)
+        print(f'A resposta é: {answer}')
+    else:
+        infer_mixed()
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Artificial-Intelligence
-Discipline of AI - UFAL
+Dupla: 
+- Matheus Levi Rodrigues Aidano
+- Giuliana Castro Diegues de Arecippo
+
+## Lista de Atividades 1
+### Pré-requisitos
+* Python
+* Instalar as dependências presentes em requirements.txt (```pip install -r requirements.txt```)
+
+### Execução
+* Preencha os arquivos dentro da pasta BaseDeConhecimento para inserir conhecimento lógico no sistema.
+* Execute o sistema com o comando ```python EngenhoDeInferencia/main.py```
